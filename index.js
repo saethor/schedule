9cda22bf949223105e39c39e5d3fa6a78008dd51
Sæþór Hallgrímsson <saethor94@gmail.com>
2015-09-02 19:29:54 +0000

    Still testing setup

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -11,5 +12,5 @@ app.get('/', function(request, response) {
 }
 
 app.listen(app.get('port'), function() {
-    console.log('Node app is running on port', app.get('port'));
-})
+  console.log('Node app is running on port', app.get('port'));
+});
