1384ed019909481de0c195d75ff84e5f33e8572f
Sæþór Hallgrímsson <saethor94@gmail.com>
2015-09-03 12:37:39 +0000

    Initial heroku commit

--- index.js
+++ index.js
@@ -5,12 +5,13 @@ app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 
-app.set('views', __dirname + 'public');
-
 app.get('/', function(request, response) {
-    response.render('index');
-}
+  response.sendFile('/index');
+  response.send();
+});
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
+
+
